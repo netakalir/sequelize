8239113babe1e21a9816b333efb86eea605bd09e
Netanel kalir <netanelkalir@gmail.com>
2025-07-14 17:08:22 +0300

    crud

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import { sequelize } from "./db.js";
 import { UserActivity } from "./models/UserActivity.js";
+import express from "express";
+import configRouts from "./routs/configRouts.js";
+import { config } from "dotenv";
+config()
+const app = express()
+app.use(express.json())
+configRouts(app)
 
 try {
-    await sequelize.sync({ force: true })
-    console.log("tabel created");
-
     const initialData = [
         { name: "Netamel", activity: "login" },
         { name: "Bob", activity: "uplode photo" },
@@ -16,6 +20,7 @@ try {
 } catch (error) {
     console.log("Err inserting data:",error);
 
-}finally{
-    await sequelize.close();
 }
+app.listen(process.env.PORT,()=>{
+    console.log(`listening on port ${process.env.PORT}`);
+})
--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 import { Sequelize, DataTypes } from "sequelize";
 
 export const sequelize = new Sequelize("sequelize_test", "root", '', {
+    logging : false,
     host: "localhost",
     dialect: "mysql",
 });
